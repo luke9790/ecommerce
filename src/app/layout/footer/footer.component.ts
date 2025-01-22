4ce1238850ad9a9c0498ad2a77b0a95f5071f1bf
lmasetti <luca9790@gmail.com>
2025-01-22 17:44:06 +0100

    fix footer

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import {
+  faInstagram,
+  faFacebook,
+  faTiktok,
+  faTwitter,
+  faCcVisa,
+  faCcMastercard,
+  faCcPaypal,
+  faCcStripe,
+  faCcAmazonPay,
+} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
   standalone: false,
-  
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent {
-
+  constructor(library: FaIconLibrary) {
+    library.addIcons(
+      faInstagram,
+      faFacebook,
+      faTiktok,
+      faTwitter,
+      faCcVisa,
+      faCcMastercard,
+      faCcPaypal,
+      faCcStripe,
+      faCcAmazonPay
+    );
+  }
 }
