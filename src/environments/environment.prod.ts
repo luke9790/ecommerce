90b9f31262d25cf4326e60b38e5a21fe92563c10
lmasetti <luca9790@gmail.com>
2025-01-31 16:52:29 +0100

    fix https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
 	production: true,
 	apiEndpoints: {
-		auth: 'http://backend-production-62bc.up.railway.app:5000/api/auth',
-		cart: 'http://backend-production-62bc.up.railway.app:5000/api/protected/cart',
-		favorites: 'http://backend-production-62bc.up.railway.app:5000/api/protected/favorites',
-		products: 'http://backend-production-62bc.up.railway.app:5000/api/products',
-		user: 'http://backend-production-62bc.up.railway.app:5000/api/protected'
+		auth: 'https://backend-production-62bc.up.railway.app:5000/api/auth',
+		cart: 'https://backend-production-62bc.up.railway.app:5000/api/protected/cart',
+		favorites: 'https://backend-production-62bc.up.railway.app:5000/api/protected/favorites',
+		products: 'https://backend-production-62bc.up.railway.app:5000/api/products',
+		user: 'https://backend-production-62bc.up.railway.app:5000/api/protected'
 	  }
 	};
   
